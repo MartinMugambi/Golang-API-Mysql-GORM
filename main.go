040776c0f168063c2dbd5665966c5d7b28491aca
Martin Mwenda <martinmwenda026@gmial.com>
2022-02-15 01:54:59 +0300

    import file to main file

--- main.go
+++ main.go
@@ -1,1 +1,20 @@
 package backend
+
+import (
+	"backend/config"
+	"backend/routes"
+	"github.com/gorilla/mux"
+	"log"
+	"net/http"
+)
+
+func main() {
+
+	router := mux.NewRouter()
+
+	config.InitializeDatabase()
+
+	routes.UserRoutes(router)
+
+	log.Fatal(http.ListenAndServe(":8000", router))
+}
