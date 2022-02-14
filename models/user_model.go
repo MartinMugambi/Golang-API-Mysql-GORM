62121af35281a9bfd1f24dcc3bed6556134a5136
Martin Mwenda <martinmwenda026@gmial.com>
2022-02-15 01:19:50 +0300

    Add User struct

--- models/user_model.go
+++ models/user_model.go
@@ -1,1 +1,11 @@
 package models
+
+import "gorm.io/gorm"
+
+type User struct {
+	gorm.Model
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+	Email     string `json:"email"`
+	Age       int    `json:"age"`
+}
