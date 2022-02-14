4abf564126b5d6ce69db29bec69efc649cba1385
Martin Mwenda <martinmwenda026@gmial.com>
2022-02-15 01:17:16 +0300

    Add set initalize database config file

--- config/database.go
+++ config/database.go
@@ -1,1 +1,23 @@
 package config
+
+import (
+	"fmt"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+var Database *gorm.DB
+
+var err error
+
+const dns = "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+
+func InitializeDatabase() {
+	Database, err = gorm.Open(mysql.Open(dns), &gorm.Config{})
+
+	if err != nil {
+		fmt.Println(err.Error())
+		panic("Could not connect to Databasr")
+	}
+
+}
