c727f0d68f12985cdc7d6c6e7a53a59b93edf009
Martin Mwenda <martinmwenda026@gmial.com>
2022-02-15 01:24:10 +0300

    Add user Routes

--- routes/user_routes.go
+++ routes/user_routes.go
@@ -1,1 +1,11 @@
 package routes
+
+import "github.com/gorilla/mux"
+
+func UserRoutes(router *mux.Router) {
+	router.HandleFunc("/users")
+	router.HandleFunc("/user/{id}")
+	router.HandleFunc("/users")
+	router.HandleFunc("/users/{id}")
+	router.HandleFunc("/users/{id}")
+}
