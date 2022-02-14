5296c22660578f95ce0e4f9bdbd1bd3b280aa0a6
Martin Mwenda <martinmwenda026@gmial.com>
2022-02-15 01:50:31 +0300

    Add controller details

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,23 +1,50 @@
 package controllers
 
-import "net/http"
+import (
+	"backend/config"
+	"backend/models"
+	"encoding/json"
+	"github.com/gorilla/mux"
+	"net/http"
+)
 
 func GetUsers(w http.ResponseWriter, r *http.Response) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var user []models.User
+	config.Database.Find(&user)
+	json.NewEncoder(w).Encode(user)
 }
 
 func GetUser(w http.ResponseWriter, r *http.Response) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user models.User
+	config.Database.First(&user, params["id"])
+	json.NewEncoder(w).Encode(user)
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Response) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var user models.User
+	json.NewDecoder(r.Body).Decode(&user)
+	config.Database.Create(&user)
+	json.NewEncoder(w).Encode(user)
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Response) {
-
+	w.Header().Set("Content-Type", "application/json")
+	params := mux.Vars(r)
+	var user models.User
+	config.Database.Find(&user, params["id"])
+	json.NewDecoder(r.Body).Decode(&user)
+	config.Database.Save(&user)
+	json.NewEncoder(w).Encode(user)
 }
 
 func DeleteUser(w http.ResponseWriter, r *http.Response) {
-
+	w.Header().Set("Content-Type", "application/json")
+	var user models.User
+	params := mux.Vars(r)
+	config.Database.Delete(&user, params["id"])
+	json.NewEncoder(w).Encode("User deleted")
 }
