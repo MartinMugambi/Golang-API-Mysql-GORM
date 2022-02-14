cb3b7b39f6eb64a7bf012945e6e1a988f7ddc6ad
Martin Mwenda <martinmwenda026@gmial.com>
2022-02-15 01:27:52 +0300

    Add user controllers

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,1 +1,23 @@
 package controllers
+
+import "net/http"
+
+func GetUsers(w http.ResponseWriter, r *http.Response) {
+
+}
+
+func GetUser(w http.ResponseWriter, r *http.Response) {
+
+}
+
+func CreateUser(w http.ResponseWriter, r *http.Response) {
+
+}
+
+func UpdateUser(w http.ResponseWriter, r *http.Response) {
+
+}
+
+func DeleteUser(w http.ResponseWriter, r *http.Response) {
+
+}
